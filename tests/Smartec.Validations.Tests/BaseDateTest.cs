2b5a4bdd626ee327180388e2e25076586fe0e02d
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-26 09:27:47 -0300

    test basedate #5

--- tests/Smartec.Validations.Tests/BaseDateTest.cs
+++ tests/Smartec.Validations.Tests/BaseDateTest.cs
@@ -31,4 +31,157 @@ public class BaseDateTest
         BaseDate? bdate = BaseDate.Now;
         Assert.NotNull(bdate);
     }
+
+    [Fact]
+    public void MinBaseDate_CheckIfIsEqualMinDateInCtor_Success()
+    {
+        BaseDate? bdate = BaseDate.MinBaseDate;
+        Assert.Equal(new BaseDate(), bdate);
+    }
+
+    [Fact]
+    public void MinBaseDate_CheckIfIsEqualNullableMinDate_Success()
+    {
+        BaseDate? bdateStaticProp = BaseDate.MinBaseDate;
+        BaseDate? bDateNewInstance = new BaseDate();
+        Assert.Equal(bdateStaticProp, bDateNewInstance);
+    }
+
+    [Fact]
+    public void MinBaseDate_CheckIfIsEqualNull_Failed()
+    {
+        BaseDate? bdate = BaseDate.MinBaseDate;
+        Assert.NotNull(bdate);
+    }
+    
+    [Fact]
+    public void MinBaseDate_CheckIfIsEqualNewInstanceMinDateTime_Success()
+    {
+        BaseDate? bdate = BaseDate.MinBaseDate;
+        Assert.Equal(new BaseDate(DateTime.MinValue.Year, DateTime.MinValue.Month), bdate);
+    }
+    
+    [Fact]
+    public void MaxBaseDate_CheckIfIsEqualNewInstanceMinDateTime_Success()
+    {
+        BaseDate? bdate = BaseDate.MaxBaseDate;
+        Assert.Equal(new BaseDate(DateTime.MaxValue.Year, DateTime.MaxValue.Month), bdate);
+    }
+
+    [Fact]
+    public void ToDateTime_TryConvert_Success()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Equal(Convert.ToDateTime(bdate), bdate.Value.ToDateTime());
+    }
+
+    [Fact]
+    public void ToBoolean_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToBoolean(bdate));
+    }
+
+    [Fact]
+    public void ToByte_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToByte(bdate));
+    }
+
+    [Fact]
+    public void ToChar_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToChar(bdate));
+    }
+
+    [Fact]
+    public void ToDecimal_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToDecimal(bdate));
+    }
+
+    [Fact]
+    public void ToDouble_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToDouble(bdate));
+    }
+
+    [Fact]
+    public void ToInt16_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToInt16(bdate));
+    }
+
+    [Fact]
+    public void ToInt32_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToInt32(bdate));
+    }
+
+    [Fact]
+    public void ToInt64_TryConvert_Failed()
+    {
+        BaseDate? bdate = new BaseDate();
+        Convert.ToInt64(bdate);
+    }
+
+    [Fact]
+    public void ToSByte_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToSByte(bdate));
+    }
+
+    [Fact]
+    public void ToSingle_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToSingle(bdate));
+    }
+
+    [Fact]
+    public void ToUInt16_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToUInt16(bdate));
+    }
+
+    [Fact]
+    public void ToUInt32_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToUInt32(bdate));
+    }
+
+    [Fact]
+    public void ToUInt64_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Assert.Throws<InvalidCastException>(
+            () => Convert.ToUInt64(bdate));
+    }
+
+    [Fact]
+    public void ToString_TryConvert_Failed()
+    {
+        BaseDate? bdate = BaseDate.Now;
+        Convert.ToString(bdate);
+    }
 }
